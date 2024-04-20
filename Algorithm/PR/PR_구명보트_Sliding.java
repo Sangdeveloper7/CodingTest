4f12655b377703d54611e1ddb68decd1b684029e
isanghyeon <leesanghyun9757@gmail.com>
2024-04-20 12:00:00 +0900

    Add sliding and spiral algorithm problems on April 20

--- Algorithm/PR/PR_구명보트_Sliding.java
+++ Algorithm/PR/PR_구명보트_Sliding.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class PR_구명보트_Sliding {
+
+    public static int solution(int[] people, int limit) {
+        int answer = 0;
+        int left = 0;
+        int right = people.length - 1;
+       Arrays.sort(people);
+
+        while(left < right) {
+            if(people[left] + people[right] > limit){
+                answer++;
+                right--;
+            }else{
+                answer++;
+                left++;
+                right--;
+            }
+        }
+        if(left == right){
+            answer++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] people = {70, 50, 80, 50};
+        int limit = 100;
+        System.out.println(solution(people, limit));
+    }
 }
--- Algorithm/PR/PR_보석쇼핑_Sliding_HashMap.java
+++ Algorithm/PR/PR_보석쇼핑_Sliding_HashMap.java
@@ -1,4 +1,59 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
 
 public class PR_보석쇼핑_Sliding_HashMap {
+
+    public static int[] solution(String[] gems) {
+        int[] answer = {};
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        for (String gem : gems) {
+            hashMap.put(gem, 0);
+        }
+        int max = hashMap.size();;
+        int min = gems.length;
+
+        int left = 0;
+        int right = 0;
+
+        int start = 1;
+        int end = gems.length ;
+        hashMap = new HashMap<>();
+
+        while(right < gems.length) {
+            if(hashMap.containsKey(gems[right])) {
+                hashMap.put(gems[right], hashMap.get(gems[right]) + 1);
+            }else{
+                hashMap.put(gems[right], 1);
+            }
+            right += 1;
+
+            while(hashMap.size() == max) {
+                if(right - left < min) {
+                    min = right - left;
+                    start = left + 1;
+                    end = right;
+                }
+                int cnt = hashMap.get(gems[left]) -1;
+                if(cnt == 0) {
+                    hashMap.remove(gems[left]);
+                }else{
+                    hashMap.put(gems[left], cnt);
+                }
+                left += 1;
+            }
+        }
+        return new int[]{start, end};
+    }
+
+    public static void main(String[] args) {
+        String[] arr = {"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"};
+        String[] arr2 = {"AA", "AB", "AC", "AA", "AC"};
+        String[] arr3 = {"XYZ", "XYZ", "XYZ"};
+
+        int[] arr4 = solution(arr);
+        for(int i : arr4) {
+            System.out.println(i);
+        }
+
+    }
 }
