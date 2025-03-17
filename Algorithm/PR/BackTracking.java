f43ab8e9324e1667404742ac30479581e6a17cd4
isanghyeon <leesanghyun9757@gmail.com>
2025-03-17 12:00:00 +0900

    🌱 Algorithm/PR/BackTracking.java 잔디 심기 커밋

--- Algorithm/PR/BackTracking.java
+++ Algorithm/PR/BackTracking.java
@@ -1,4 +1,27 @@
-package 취업준비;
-
 public class BackTracking {
+
+    private static final char[] alpha = {'A', 'B', 'C', 'D'};
+
+    private static final int LENGTH = 4;
+
+    private static boolean[] visited = new boolean[4];
+
+    public static void main(String[] args) {
+        backTracking("");
+    }
+
+    private static void backTracking(String s) {
+        if(s.length() == LENGTH) {
+            System.out.println(s);
+            return;
+        }
+
+        for(int i = 0; i < 4; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                backTracking(s + alpha[i]);
+                visited[i] = false; // 한번씩만 사용 할 경우 모든 경우의 수 출력
+            }
+        }
+    }
 }
