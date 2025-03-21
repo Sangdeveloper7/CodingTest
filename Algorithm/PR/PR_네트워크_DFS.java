0ec1132a9e3e4f0b7179d9e48c0beed16abbb325
isanghyeon <leesanghyun9757@gmail.com>
2025-03-21 12:00:00 +0900

    🌱 Algorithm/PR/PR_네트워크_DFS.java 잔디 심기 커밋

--- Algorithm/PR/PR_네트워크_DFS.java
+++ Algorithm/PR/PR_네트워크_DFS.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+public class PR_네트워크_DFS {
 
-public class PR_네트워크 {
+    private static int[][] computer;
+    private static boolean[] visited;
+
+    public static int solution(int n, int[][] computers) {
+        computer = computers;
+
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                dfs(i);
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    private static void dfs(int now){
+        if(visited[now]){
+            return;
+        }else{
+           visited[now] = true;
+
+           for(int i = 0; i<computer.length; i++){
+               if(computer[now][i] == 1)
+                   dfs(i);
+
+           }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
