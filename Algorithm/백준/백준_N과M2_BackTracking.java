efaeaab30ecdec2a91d87b75c7d453d8f54fafa9
isanghyeon <leesanghyun9757@gmail.com>
2025-03-27 12:00:00 +0900

    🌱 Algorithm/백준/백준_N과M2_BackTracking.java 잔디 심기 커밋

--- Algorithm/백준/백준_N과M2_BackTracking.java
+++ Algorithm/백준/백준_N과M2_BackTracking.java
@@ -3,22 +3,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class 백준_N과M1_BackTracking {
+public class 백준_N과M2_BackTracking {
 
     private static int N, M;
     private static boolean[] check;
     private static StringBuilder sb = new StringBuilder();
 
-    private static void backtracking(int cnt, String s) {
+    private static void backtracking(int number, int cnt, String s) {
         if(cnt == M) {
             sb.append(s.trim() + "\n");
             return;
         }
 
-        for(int i = 1; i<=N; i++) {
+        for(int i = number + 1; i<=N; i++) {
             if(!check[i]) {
                 check[i] = true;
-                backtracking(cnt + 1, s + " " + i);
+                backtracking(i,cnt + 1, s + " " + i);
                 check[i] = false;
             }
         }
@@ -32,7 +32,7 @@ public class 백준_N과M1_BackTracking {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         check = new boolean[N+1];
-        backtracking(0, "");
+        backtracking(0,0, "");
         System.out.println(sb);
 
     }
