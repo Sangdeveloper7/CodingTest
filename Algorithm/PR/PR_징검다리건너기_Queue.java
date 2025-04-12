73b6db24cd47658c5d170f3385a4bcf42548365b
isanghyeon <leesanghyun9757@gmail.com>
2025-04-12 22:29:08 +0900

    4/12 CodingTest

---
+++ Algorithm/PR/PR_징검다리건너기_Queue.java
@@ -0,0 +1,43 @@
+import java.security.PublicKey;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class PR_징검다리건너기_Queue {
+    private static class Stone{
+        int value;
+        int index;
+
+    public Stone(int value, int index) {
+        this.value = value;
+        this.index = index;
+    }
+
+    }
+
+    public static int solution(int[] stones, int k) {
+        PriorityQueue<Stone> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o2.value,o1.value));
+        int i = 0;
+        for (i = 0; i < k; i++) {
+            pq.add(new Stone(stones[i], i));
+        }
+        int answer = pq.peek().value;
+
+        while(i < stones.length) {
+            pq.add(new Stone(stones[i], i));
+            i++;
+            while(pq.peek().index < i - k){
+                pq.poll();
+            }
+            answer = Math.min(answer, pq.peek().value);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] stone = {2, 4, 5, 3, 2, 1, 4, 2, 5, 1};
+        int k = 3;
+        System.out.println(solution(stone, k));
+    }
+}
