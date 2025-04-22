7157774648f48e100e72cb40e4c22bb1ce42e429
isanghyeon <leesanghyun9757@gmail.com>
2025-04-22 21:09:31 +0900

    4/22 CodingTest

--- Algorithm/PR/PR_평행.java
+++ Algorithm/PR/PR_평행.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+import org.w3c.dom.Node;
+
+import java.util.HashSet;
 
 public class PR_평행 {
+    private static class Node {
+        int x;
+        int y;
+        Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static int solution(int[][] dots) {
+        int x1_in = 0;
+        int y1_in = 0;
+        int x2_in = 0;
+        int y2_in = 0;
+
+        HashSet<Double> set = new HashSet<>();
+        int count = 0;
+
+        for(int i = 0; i < dots.length; i++) {
+            Node n = new Node(dots[i][0], dots[i][1]);
+            for(int j = i+1; j < dots.length; j++) {
+                Node n2 = new Node(dots[j][0], dots[j][1]);
+                double incline = (double) (n.y - n2.y) / (n.x - n2.x);
+                set.add(incline);
+                count++;
+            }
+        }
+        if(set.size() == count) {
+           return 0;
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        int[][] dots = {{1, 4}, {2, 1}, {3, 2}, {4, 7}};
+    }
 }
--- Algorithm/백준/백준_수열2559_SlidingWindow.java
+++ Algorithm/백준/백준_수열2559_SlidingWindow.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class 백준_수열2559_SlidingWindow {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] nums = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        int sum = 0;
+        for(int i =0; i< K; i++){
+            sum += nums[i];
+        }
+        int answer = sum;
+
+        int left = 0;
+        int right = K;
+
+        while(right < N){
+            sum += nums[right++];
+            sum -= nums[left++];
+            answer = Math.max(answer, sum);
+        }
+        System.out.println(answer);
+
+
+    }
 }
