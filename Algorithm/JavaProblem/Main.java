e3a601f43b6b922b19388c5ba807758c8c825398
이상현 <leesanghyun9757@gmail.com>
2025-07-02 15:53:47 +0900

    7/2 Java CodingTest D  series

--- Algorithm/JavaProblem/Main.java
+++ Algorithm/JavaProblem/Main.java
@@ -5,11 +5,11 @@ import org.w3c.dom.ls.LSOutput;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        C11();
+        D10();
     }
 
     static void A01() throws IOException {
@@ -391,4 +391,208 @@ public class Main {
             System.out.println("This year is not leap year\n");
         }
     }
+
+    static void D01() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter a number > ");
+        int number = Integer.parseInt(br.readLine());
+        if(number <= 1)
+            System.out.println("Invalid input");
+        else{
+            int totalsum = number * (number+1) / 2;
+            System.out.println("The sum of all integer from 1 to the number entered is " + totalsum);
+        }
+    }
+
+    static void D02() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> arr = new ArrayList<>();
+        while(true){
+            System.out.print("Enter a number > ");
+            int number = Integer.parseInt(br.readLine());
+            if(number < 0 || number > 100)
+                break;
+            else
+                arr.add(number);
+        }
+        Collections.sort(arr);
+        int max_num = arr.get(arr.size()-1);
+        int min_num = arr.get(0);
+        System.out.println("Max number is " + max_num + " , Min number is " + min_num);
+    }
+
+    static void D03() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int total_sum = 0;
+        int count = 0;
+        while(true){
+            System.out.print("Enter a number > ");
+            int number = Integer.parseInt(br.readLine());
+            if(number < 0 || number > 100)
+                break;
+            total_sum += number;
+            count++;
+        }
+        System.out.println("The sum of " + count + " numbers is " + total_sum + " , average is " + (float) total_sum / count);
+    }
+
+    static void D04() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter number of family members > ");
+        int count_all = Integer.parseInt(br.readLine());
+        int count_young = 0;
+
+        for(int i = 0; i < count_all; i++){
+            int birth_year = Integer.parseInt(br.readLine());
+            int age = 2021 - birth_year + 1;
+            if(age < 20) count_young++;
+        }
+
+        System.out.println("There are " + count_young + " youngs in the family. ");
+    }
+
+    static void D05() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int width, height;
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
+        int count4 = 0;
+        int count5 = 0;
+
+        while (true) {
+            System.out.print("Enter width, height of Rectangle > ");
+            String[] input = br.readLine().split(" ");
+            width = Integer.parseInt(input[0]);
+            height = Integer.parseInt(input[1]);
+
+            if (width <= 0 || height <= 0) break;
+            if (width == height) {
+                count1++;
+            } else if (width >= height * 2) {
+                count2++;
+            } else if (height >= width * 2) {
+                count3++;
+            } else if (width > height) {
+                count4++;
+            } else {
+                count5++;
+            }
+        }
+
+        System.out.println("Count of square is " + count1);
+        System.out.println("Count of an oblong rectangle from side to side is " + count2);
+        System.out.println("Count of an oblong rectangle from top to bottom is " + count3);
+        System.out.println("Count of a horizontal rectangle is " + count4);
+        System.out.println("Count of a vertical rectangle is " + count5);
+    }
+
+    static void D06() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double m2_area;
+        double pyung_area;
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
+        int count4 = 0;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter area of Apartment(m^2) > ");
+            m2_area = Double.parseDouble(br.readLine());
+            pyung_area = m2_area / 3.305;
+            if (pyung_area < 15)
+                count1++;
+            else if (pyung_area < 30)
+                count2++;
+            else if (pyung_area < 50)
+                count3++;
+            else
+                count4++;
+        }
+
+        System.out.println("Count of small-sized apartment is " + count1);
+        System.out.println("Count of small and medium-sized apartment is " + count2);
+        System.out.println("Count of medium-sized apartment is " + count3);
+        System.out.println("Count of big-sized apartment is " + count4);
+    }
+
+    static void D07() throws IOException{
+        System.out.print("Enter the coefficients a and b of linear function(y=ax+b) > ");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.print("Enter the start and end values of x coordinates > ");
+
+        st = new StringTokenizer(br.readLine());
+        int x_begin = Integer.parseInt(st.nextToken());
+        int x_end = Integer.parseInt(st.nextToken());
+
+        for(int i = x_begin; i < x_end; i++){
+            int y = a * i + b;
+            int x = i;
+            System.out.println("coordinates (" + x + " ," + y +")");
+        }
+    }
+
+    static void D08()  throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter the coefficients a and b of quadratic function(y=ax^2+bx+c) > ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        System.out.print("Enter the start and end values of x coordinates > ");
+        st = new StringTokenizer(br.readLine());
+
+        int x_begin = Integer.parseInt(st.nextToken());
+        int x_end = Integer.parseInt(st.nextToken());
+
+        for (int x = x_begin; x <= x_end; x++) {
+            int y = a * x * x + b * x + c;
+            System.out.println("coordinates (" + x + ", " + y + ")");
+        }
+    }
+
+    static void D09()  throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int dan;
+
+        while (true) {
+            System.out.print("Enter number of gugudan(2~9) > ");
+            dan = Integer.parseInt(br.readLine());
+            if (dan >= 2 && dan <= 9) {
+                break;
+            } else {
+                System.out.println("Invalid input");
+            }
+        }
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(dan + " X " + i + " = " + (dan * i));
+        }
+    }
+
+    static void D10()  throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter two numbers > ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+
+        for(int i = 1; i <= 100; i++){
+            if(i % num1 == 0 && i % num2 == 0){
+                continue;
+            }else if(i % num1 == 0 || i % num2 == 0)
+                System.out.print(i + " ");
+        }
+    }
+
+
 }
